fccf6b9e5264a2875436df0da415cded800facf7
sakazuki <sakazuki@gmail.com>
2015-02-12 08:09:37 +0900

    start

---
+++ lib/chef/formatters/simple.rb
@@ -0,0 +1,9 @@
+require "chef/formatters/simple/version"
+
+module Chef
+  module Formatters
+    module Simple
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/chef/formatters/simple/version.rb
@@ -0,0 +1,7 @@
+module Chef
+  module Formatters
+    module Simple
+      VERSION = "0.0.1"
+    end
+  end
+end
